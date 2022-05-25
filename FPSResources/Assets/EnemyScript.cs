b592ba5fc9cedf94287fc6f16931bf4fcc2b4a9e
Linh <linhnguyen1326@gmail.com>
2022-05-25 12:50:34 +0700

    update source

---
+++ FPSResources/Assets/EnemyScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Die();
+    }
+
+
+    void Die()
+    {
+        if(health <= 0)
+        {
+     
+            Destroy(gameObject);
+        }
+    }
+}
